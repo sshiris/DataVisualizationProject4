9b8562fa55809246f640ddcf2e0dce5336f4ad9c
Shchennikov, Daniil <e2301469@edu.vamk.fi>
2025-09-18 12:38:43 +0000

    Merged PR 5: requirements updated

---
+++ server/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.root_node import router as get_root
+from routes.child_node import router as get_child
+
+app = FastAPI()
+
+# allowing everything for dev environment
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"]
+)
+
+app.include_router(get_root, prefix='/api')
+app.include_router(get_child, prefix='/api')
+    
