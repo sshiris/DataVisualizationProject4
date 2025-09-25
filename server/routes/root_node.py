f7c0b828ffc120a8e6b8792caa6422fcc752fad5
Iris <148431646+iris915520@users.noreply.github.com>
2025-09-25 03:08:25 +0300

    update

--- server/routes/root_node.py
+++ server/routes/root_node.py
@@ -1,15 +1,7 @@
 from fastapi import APIRouter
+from data.sample_data import sample_data
 
 router = APIRouter()
-
-sample_data = [
-    {"parent_item": "MAT000001", "child_item": "MAT000004", "sequence_no": 1, "level": 1},
-    {"parent_item": "MAT000001", "child_item": "MAT000007", "sequence_no": 10, "level": 1},
-    {"parent_item": "MAT000001", "child_item": "MAT000008", "sequence_no": 11, "level": 1},
-    {"parent_item": "MAT000002", "child_item": "MAT000017", "sequence_no": 1, "level": 2},
-    {"parent_item": "MAT000004", "child_item": "MAT000018", "sequence_no": 1, "level": 2},
-]
-
 @router.get("/root_node")
 async def get_root_node():
     parents = set()
